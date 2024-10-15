5604530b6646a639014fbd78f6a9b673f47eaefa
unknown <edmondsimionescu@yahoo.com>
2024-10-15 16:18:19 +0300

    Lab_3

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Lab 2")
+# a = 0xFFFFFFFF
+# bitNum = int(input("bit number? :"))
+# result = a & (~0b00&~(0b01<<bitNum-1))
+# print("{:032b}".format(result))
+
+sir = "iLikePython"
+print(sir[::-1])
